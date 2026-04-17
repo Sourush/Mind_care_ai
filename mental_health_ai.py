44d01ffca7ada044f0c66e4097025d7097bf56f6
Sourush Guleria <sourushg97@gmail.com>
2026-04-17 23:12:09 +0530

    fixed port for deploy

--- mental_health_ai.py
+++ mental_health_ai.py
@@ -207,4 +207,4 @@ def chat():
 # ------------------ RUN ------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
